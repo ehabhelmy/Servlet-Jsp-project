c6d54646e6616925477ac5dcb589db873cc47cf7
ehabm <ehabm@DESKTOP-IUGPO0B>
2017-02-26 23:19:40 +0200

    start of our project added mysql driver version 6.3 and user and product dtos ara avaialable with databaseHandler

---
+++ ServletProject/src/main/java/com/tnaneen/servletproject/Product.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tnaneen.servletproject;
+
+/**
+ *
+ * @author ehabm
+ */
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+    private int available;
+    private String category;
+    private String description;
+    private String image;
+    private String productcol;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getProductcol() {
+        return productcol;
+    }
+
+    public void setProductcol(String productcol) {
+        this.productcol = productcol;
+    }
+    
+    
+}
