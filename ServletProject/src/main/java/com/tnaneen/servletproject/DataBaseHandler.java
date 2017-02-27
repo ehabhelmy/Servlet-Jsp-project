c1fa0a6d4733e7f02d37571dbab0f9a2de739ce3
ehabm <ehabm@DESKTOP-IUGPO0B>
2017-02-27 18:39:38 +0200

    start of our project added mysql driver version 6.3 and user and product dtos ara avaialable with databaseHandler with open and close connection

--- ServletProject/src/main/java/com/tnaneen/servletproject/DataBaseHandler.java
+++ ServletProject/src/main/java/com/tnaneen/servletproject/DataBaseHandler.java
@@ -5,14 +5,45 @@
  */
 package com.tnaneen.servletproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ehabm
  */
 public class DataBaseHandler {
-    
-    private static void openConnection()
-    {
-        
+
+    private final String URL = "jdbc:mysql://localhost/ecommerce";
+    private final String userName = "ehab";
+    private final String password = "ehab";
+    private Connection conn;
+    private PreparedStatement pst;
+    private ResultSet rs;
+
+    private void openConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, userName, password);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void closeConnection() {
+        try {
+            pst.close();
+            rs.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
